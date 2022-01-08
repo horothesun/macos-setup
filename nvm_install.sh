b978b4d8bc774f3a8b0e3cebd8852442c478efa8
horothesun <horothesun@gmail.com>
2022-01-08 01:02:41 +0000

    nvm setup fix

--- nvm_install.sh
+++ nvm_install.sh
@@ -2,8 +2,20 @@
 
 brew install nvm
 
+
+# HOMEBREW_OPT_DIR: different values between Intel and Apple Silicon (arm64)
+if [ "`arch`" = arm64 ]; then
+  export HOMEBREW_OPT_DIR=/opt/homebrew/opt
+else
+  export HOMEBREW_OPT_DIR=/usr/local/opt
+fi
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$HOMEBREW_OPT_DIR/nvm/nvm.sh" ] && . "$HOMEBREW_OPT_DIR/nvm/nvm.sh"
+[ -s "$HOMEBREW_OPT_DIR/nvm/etc/bash_completion.d/nvm" ] && . "$HOMEBREW_OPT_DIR/nvm/etc/bash_completion.d/nvm"
+
 nvm install --lts --default
 
 echo
-echo 'Node.js version'
-nvm version
+echo 'Node.js versions:'
+nvm list
