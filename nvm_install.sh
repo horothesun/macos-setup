c9cc3b83144e5225f9d0232676ae5c17c54dac5c
horothesun <horothesun@gmail.com>
2022-01-08 15:41:55 +0000

    nvm setup simplified

--- nvm_install.sh
+++ nvm_install.sh
@@ -4,11 +4,7 @@ brew install nvm
 
 
 # HOMEBREW_OPT_DIR: different values between Intel and Apple Silicon (arm64)
-if [ "`arch`" = arm64 ]; then
-  export HOMEBREW_OPT_DIR=/opt/homebrew/opt
-else
-  export HOMEBREW_OPT_DIR=/usr/local/opt
-fi
+export HOMEBREW_OPT_DIR=$(brew --prefix)/opt
 
 # Initialise nvm
 export NVM_DIR="$HOME/.nvm"
