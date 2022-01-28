2b17ad523b7984790ff7f3e253685e527e4c7921
horothesun <horothesun@gmail.com>
2022-01-28 10:37:07 +0000

    rbenv install cleanup

--- rbenv_install.sh
+++ rbenv_install.sh
@@ -5,9 +5,10 @@ brew install rbenv
 rbenv install --list-all
 
 rbenv install 2.7.5
-rbenv install 3.1.0
 
-rbenv global 3.1.0
+RUBY_GLOBAL_VERSION=3.1.0
+rbenv install $RUBY_GLOBAL_VERSION
+rbenv global $RUBY_GLOBAL_VERSION
 
 echo
 echo 'Ruby versions:'
