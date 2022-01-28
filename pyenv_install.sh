39bdb654a5fee509fe36f19607ffb097471ca2c1
horothesun <horothesun@gmail.com>
2022-01-28 10:37:25 +0000

    pyenv install cleanup

--- pyenv_install.sh
+++ pyenv_install.sh
@@ -4,9 +4,9 @@ brew install pyenv
 
 pyenv install --list
 
-pyenv install 3.10.1
-
-pyenv global 3.10.1
+PYTHON_GLOBAL_VERSION=3.10.2
+pyenv install $PYTHON_GLOBAL_VERSION
+pyenv global $PYTHON_GLOBAL_VERSION
 
 echo
 echo 'Python versions:'
