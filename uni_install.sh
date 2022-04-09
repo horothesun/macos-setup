ce53603d544259fa71257516676c59a63478f484
horothesun <horothesun@gmail.com>
2022-04-09 13:31:29 +0100

    uni install script: embedded re-install logic

--- uni_install.sh
+++ uni_install.sh
@@ -4,11 +4,11 @@
 # - go compiler
 # - golang.org/x/sys go library
 
-cd ~
+UNI_CLONE_PATH=~/arp242-uni
 
-git clone https://github.com/arp242/uni
+[ -d "$UNI_CLONE_PATH" ] && echo "$UNI_CLONE_PATH folder already exists. Removing it..." && rm -fr $UNI_CLONE_PATH
 
-cd uni
-
+git clone https://github.com/arp242/uni $UNI_CLONE_PATH
+cd $UNI_CLONE_PATH
 go get -u golang.org/x/sys
 go build
