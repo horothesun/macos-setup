cc53fd4351bf03f521b4b3feecf95cb3f7da4348
horothesun <horothesun@gmail.com>
2022-05-29 00:56:33 +0100

    pyenv_install.sh: python version update

--- pyenv_install.sh
+++ pyenv_install.sh
@@ -4,10 +4,10 @@ brew install pyenv
 
 pyenv install --list
 
-PYTHON_GLOBAL_VERSION=3.10.2
+PYTHON_GLOBAL_VERSION=3.10.4
 pyenv install $PYTHON_GLOBAL_VERSION
 pyenv global $PYTHON_GLOBAL_VERSION
 
 echo
 echo 'Python versions:'
-pyenv version
+pyenv versions
