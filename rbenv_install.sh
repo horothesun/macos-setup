3e1f9ed84cc623f30ad186fae0c00a32b7604c7d
horothesun <horothesun@gmail.com>
2022-05-29 00:49:10 +0100

    rbenv_install.sh: ruby versions update

--- rbenv_install.sh
+++ rbenv_install.sh
@@ -4,9 +4,9 @@ brew install rbenv
 
 rbenv install --list-all
 
-rbenv install 2.7.5
+rbenv install 2.7.6
 
-RUBY_GLOBAL_VERSION=3.1.1
+RUBY_GLOBAL_VERSION=3.1.2
 rbenv install $RUBY_GLOBAL_VERSION
 rbenv global $RUBY_GLOBAL_VERSION
 
