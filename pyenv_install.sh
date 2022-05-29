0aa5b0fab19eca1bb6842ce9694a96c0b2aec468
horothesun <horothesun@gmail.com>
2022-05-29 11:36:16 +0100

    pyenv_install.sh: automaticly retrieve latest Python stable version

--- pyenv_install.sh
+++ pyenv_install.sh
@@ -4,9 +4,17 @@ brew install pyenv
 
 pyenv install --list
 
-PYTHON_GLOBAL_VERSION=3.10.4
-pyenv install "${PYTHON_GLOBAL_VERSION}"
-pyenv global "${PYTHON_GLOBAL_VERSION}"
+PYTHON_LATEST_STABLE_V3=$(
+  pyenv install --list | \
+    grep -v "-" | \
+    grep -v "[a-z]" | \
+    xargs -n 1 | \
+    grep "^3" | \
+    tail -1
+)
+pyenv install "${PYTHON_LATEST_STABLE_V3}"
+pyenv global "${PYTHON_LATEST_STABLE_V3}"
+pyenv rehash
 
 echo
 echo "Python versions:"
