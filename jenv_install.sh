42d924f7b1d92f09ee530a8b90f1f51353171254
horothesun <horothesun@gmail.com>
2022-05-29 01:44:50 +0100

    Cleanup

--- jenv_install.sh
+++ jenv_install.sh
@@ -2,7 +2,7 @@
 
 brew install jenv
 
-mkdir -p ~/.jenv/versions
+mkdir -p "${HOME}/.jenv/versions"
 
 # https://github.com/AdoptOpenJDK/homebrew-openjdk
 brew tap homebrew/cask-versions
@@ -10,9 +10,9 @@ brew tap homebrew/cask-versions
 brew install --cask temurin8 temurin11 temurin
 
 echo
-echo 'JDK versions:'
+echo "JDK versions:"
 /usr/libexec/java_home -V
 
 echo
-echo 'jEnv versions:'
+echo "jEnv versions:"
 jenv versions
--- nvm_install.sh
+++ nvm_install.sh
@@ -5,7 +5,6 @@ brew install nvm
 
 # HOMEBREW_OPT_DIR: different values between Intel and Apple Silicon (arm64)
 HOMEBREW_OPT_DIR="$(brew --prefix)/opt"
-export HOMEBREW_OPT_DIR
 
 # Initialise nvm
 NVM_DIR="${HOME}/.nvm"
