21b253f9c9d2bf3966ad15533b1dd9c0cfb5663c
horothesun <horothesun@gmail.com>
2022-05-29 01:38:08 +0100

    install.sh: shellcheck fix

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ FORMULAE_FILE="formulae"
 echo "Installing the following formulae:"
 cat "${FORMULAE_FILE}"
 echo
-brew install $(cat "${FORMULAE_FILE}")
+brew install "$(cat "${FORMULAE_FILE}")"
 
 
 # fonts
@@ -14,7 +14,7 @@ FONTS_FILE="fonts"
 echo "Installing the following fonts:"
 cat "${FONTS_FILE}"
 echo
-brew install $(cat "${FONTS_FILE}")
+brew install "$(cat "${FONTS_FILE}")"
 
 
 # casks
@@ -22,16 +22,16 @@ CASKS_FILE="casks"
 echo "Installing the following casks:"
 cat "${CASKS_FILE}"
 echo
-brew install --cask $(cat "${CASKS_FILE}")
+brew install --cask "$(cat "${CASKS_FILE}")"
 
 
 # Intel-only casks
-if [ "$(arch)" != arm64 ]; then
+if [[ "$(arch)" != arm64 ]]; then
   INTEL_ONLY_CASKS_FILE="intel_only_casks"
   echo "Installing the following Intel-only casks:"
   cat "${INTEL_ONLY_CASKS_FILE}"
   echo
-  brew install --cask $(cat "${INTEL_ONLY_CASKS_FILE}")
+  brew install --cask "$(cat "${INTEL_ONLY_CASKS_FILE}")"
 fi
 
 
