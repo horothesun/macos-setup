d7e8c0e6d07e6a4ac9848f01c20acf92f34b54ff
horothesun <horothesun@gmail.com>
2022-05-29 01:31:50 +0100

    pass_install.sh: shellcheck fix

--- pass_install.sh
+++ pass_install.sh
@@ -4,11 +4,11 @@ brew install pass
 brew install amar1729/formulae/browserpass
 
 # PREFIX: different values between Intel and Apple Silicon (arm64)
-export PREFIX=`brew --prefix`
+PREFIX=$(brew --prefix)
 
 # Reference: https://github.com/browserpass/browserpass-native#configure-browsers
-make -f $PREFIX/lib/browserpass/Makefile \
-  hosts-chrome-user \
-  hosts-chromium-user \
-  hosts-brave-user \
-  hosts-firefox-user
+make -f "${PREFIX}/lib/browserpass/Makefile" \
+  "hosts-chrome-user" \
+  "hosts-chromium-user" \
+  "hosts-brave-user" \
+  "hosts-firefox-user"
