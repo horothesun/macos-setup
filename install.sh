558a3072948217791f98945d7429779bf50d32ec
horothesun <horothesun@gmail.com>
2022-05-29 01:35:34 +0100

    install.sh: shellcheck fix

--- install.sh
+++ install.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
 
 # formulae
-FORMULAE_FILE=formulae
+FORMULAE_FILE="formulae"
 echo "Installing the following formulae:"
-cat $FORMULAE_FILE
+cat "${FORMULAE_FILE}"
 echo
-brew install `cat $FORMULAE_FILE`
+brew install $(cat "${FORMULAE_FILE}")
 
 
 # fonts
 brew tap homebrew/cask-fonts
-FONTS_FILE=fonts
+FONTS_FILE="fonts"
 echo "Installing the following fonts:"
-cat $FONTS_FILE
+cat "${FONTS_FILE}"
 echo
-brew install `cat $FONTS_FILE`
+brew install $(cat "${FONTS_FILE}")
 
 
 # casks
-CASKS_FILE=casks
+CASKS_FILE="casks"
 echo "Installing the following casks:"
-cat $CASKS_FILE
+cat "${CASKS_FILE}"
 echo
-brew install --cask `cat $CASKS_FILE`
+brew install --cask $(cat "${CASKS_FILE}")
 
 
 # Intel-only casks
-if [ "`arch`" != arm64 ]; then
-  INTEL_ONLY_CASKS_FILE=intel_only_casks
+if [ "$(arch)" != arm64 ]; then
+  INTEL_ONLY_CASKS_FILE="intel_only_casks"
   echo "Installing the following Intel-only casks:"
-  cat $INTEL_ONLY_CASKS_FILE
+  cat "${INTEL_ONLY_CASKS_FILE}"
   echo
-  brew install --cask `cat $INTEL_ONLY_CASKS_FILE`
+  brew install --cask $(cat "${INTEL_ONLY_CASKS_FILE}")
 fi
 
 
