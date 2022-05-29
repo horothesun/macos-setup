aee21a3c18d9ce309068463da3176b4241d0c048
horothesun <horothesun@gmail.com>
2022-05-29 01:08:49 +0100

    uni_install.sh: shellcheck fix

--- uni_install.sh
+++ uni_install.sh
@@ -4,11 +4,11 @@
 # - go compiler
 # - golang.org/x/sys go library
 
-UNI_CLONE_PATH=~/arp242-uni
+UNI_CLONE_PATH="~/arp242-uni"
 
-[ -d "$UNI_CLONE_PATH" ] && echo "$UNI_CLONE_PATH folder already exists. Removing it..." && rm -fr $UNI_CLONE_PATH
+[[ -d "${UNI_CLONE_PATH}" ]] && echo "${UNI_CLONE_PATH} folder already exists. Removing it..." && rm -fr "${UNI_CLONE_PATH}"
 
-git clone https://github.com/arp242/uni $UNI_CLONE_PATH
-cd $UNI_CLONE_PATH
-go get -u golang.org/x/sys
+git clone "https://github.com/arp242/uni" "${UNI_CLONE_PATH}"
+cd "${UNI_CLONE_PATH}"
+go get -u "golang.org/x/sys"
 go build
