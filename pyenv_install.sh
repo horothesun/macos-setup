fd17b575f6fbc5de481181acfde9ddc851ee0b92
horothesun <horothesun@gmail.com>
2022-05-29 01:28:35 +0100

    pyenv_install.sh: shellcheck fix

--- pyenv_install.sh
+++ pyenv_install.sh
@@ -5,9 +5,9 @@ brew install pyenv
 pyenv install --list
 
 PYTHON_GLOBAL_VERSION=3.10.4
-pyenv install $PYTHON_GLOBAL_VERSION
-pyenv global $PYTHON_GLOBAL_VERSION
+pyenv install "${PYTHON_GLOBAL_VERSION}"
+pyenv global "${PYTHON_GLOBAL_VERSION}"
 
 echo
-echo 'Python versions:'
+echo "Python versions:"
 pyenv versions
