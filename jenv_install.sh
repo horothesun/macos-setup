d6b126862be5abe8f76d010586d117c24303695a
horothesun <horothesun@gmail.com>
2022-10-08 12:00:03 +0100

    jenv_install: temurin18 added

--- jenv_install.sh
+++ jenv_install.sh
@@ -35,6 +35,11 @@ mkdir -p "${HOME}/.jenv/versions"
 # https://github.com/AdoptOpenJDK/homebrew-openjdk
 brew tap homebrew/cask-versions
 
-brew install --cask temurin8 temurin11 temurin17 temurin
+brew install --cask \
+  temurin \
+  temurin18 \
+  temurin17 \
+  temurin11 \
+  temurin8
 
 jenv_reset
